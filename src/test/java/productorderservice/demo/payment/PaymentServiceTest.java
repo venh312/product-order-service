4ce23911bb7dfc7782f3babe14004023215cd553
conf312 <conf312@naver.com>
2023-11-13 12:15:12 +0900

    [ADD] 스프링부트 테스트로 전환하기 - 주문 결제 기능 구현하기 (#11)
    Co-authored-by: VenhMac <gpfm312@gmail.com>

--- src/test/java/productorderservice/demo/payment/PaymentServiceTest.java
+++ src/test/java/productorderservice/demo/payment/PaymentServiceTest.java
@@ -1,25 +1,28 @@
 package productorderservice.demo.payment;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import productorderservice.demo.order.OrderService;
+import productorderservice.demo.order.OrderSteps;
+import productorderservice.demo.product.ProductSteps;
+import productorderservice.demo.product.service.ProductService;
 
+@SpringBootTest
 public class PaymentServiceTest {
-
-    private final PaymentSteps paymentSteps = new PaymentSteps();
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private OrderService orderService;
+    @Autowired
     private PaymentService paymentService;
-    private PaymentPort paymentPort;
-
-    @BeforeEach
-    void setUp() {
-        PaymentGateway paymentGateway = new ConsolePaymentGateway();
-        PaymentRepository paymentRepository = new PaymentRepository();
-        paymentPort = new PaymentAdapter(paymentGateway, paymentRepository);
-        paymentService = new PaymentService(paymentPort);
-    }
 
     @Test
     void 상품주문() {
-        PaymentRequest request = paymentSteps.주문결제요청_생성();
+        productService.addProduct(ProductSteps.상품등록요청_생성());
+        orderService.createOrder(OrderSteps.상품주문요청_생성());
+
+        PaymentRequest request = PaymentSteps.주문결제요청_생성();
         paymentService.payment(request);
     }
 }
--- src/test/java/productorderservice/demo/payment/PaymentSteps.java
+++ src/test/java/productorderservice/demo/payment/PaymentSteps.java
@@ -1,7 +1,7 @@
 package productorderservice.demo.payment;
 
 public class PaymentSteps {
-    PaymentRequest 주문결제요청_생성() {
+    public static PaymentRequest 주문결제요청_생성() {
         Long orderId = 1L;
         String cardNumber = "1100-2302-0032";
         return new PaymentRequest(orderId, cardNumber);
