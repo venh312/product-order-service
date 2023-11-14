a8da2797f0e3e007994fd6bbb2406c76a80a5873
VenhMac <gpfm312@gmail.com>
2023-11-14 12:12:23 +0900

    [ADD] API 테스트로 전환하기 - 주문 결제 기능 구현하기

--- src/test/java/productorderservice/demo/payment/PaymentService.java
+++ src/test/java/productorderservice/demo/payment/PaymentService.java
@@ -1,9 +1,15 @@
 package productorderservice.demo.payment;
 
-import org.springframework.stereotype.Component;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import productorderservice.demo.order.Order;
 
-@Component
+@RestController
+@RequestMapping(value = "payments")
 class PaymentService {
     private final PaymentPort paymentPort;
 
@@ -11,10 +17,13 @@ class PaymentService {
         this.paymentPort = paymentPort;
     }
 
-    public void payment(PaymentRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> payment(@RequestBody PaymentRequest request) {
         Order order = paymentPort.getOrder(request.orderId());
         Payment payment = new Payment(order, request.cardNumber());
         paymentPort.pay(payment.getPrice(), payment.getCardNumber());
         paymentPort.save(payment);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
--- src/test/java/productorderservice/demo/payment/PaymentSteps.java
+++ src/test/java/productorderservice/demo/payment/PaymentSteps.java
@@ -1,9 +1,24 @@
 package productorderservice.demo.payment;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
 public class PaymentSteps {
     public static PaymentRequest 주문결제요청_생성() {
         Long orderId = 1L;
         String cardNumber = "1100-2302-0032";
         return new PaymentRequest(orderId, cardNumber);
+    }
+
+    public static ExtractableResponse<Response> 주문결제요청(PaymentRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/payments")
+                .then().log().all()
+                .extract();
     }
 }
